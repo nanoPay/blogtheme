ad5ba2e790346dd62e0e3f7cfbeca8eb3bbc67ff
Joe Auty <joe@netmusician.org>
2014-10-03 10:14:11 -0400

    initial theme commit

---
+++ sparkling/header.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package sparkling
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<!-- favicon -->
+
+<?php if ( of_get_option( 'custom_favicon' ) ) { ?>
+<link rel="icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" />
+<?php } ?>
+
+<!--[if IE]><?php if ( of_get_option( 'custom_favicon' ) ) { ?><link rel="shortcut icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" /><?php } ?><![endif]-->
+
+<!-- googleanalytics -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46019584-3', 'nanopay.net');
+  ga('send', 'pageview');
+
+</script>
+
+<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+
+	<header id="masthead" class="site-header" role="banner">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<div class="row">
+                                        
+					<div class="site-navigation-inner col-sm-12">
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+
+				<?php if( get_header_image() != '' ) : ?>
+                                 
+					<div id="logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>"  height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+					</div><!-- end of #logo -->
+
+				<?php endif; // header image was removed ?>
+
+				<?php if( !get_header_image() ) : ?>
+
+					<div id="logo">
+						<span class="site-name"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+					</div><!-- end of #logo -->
+
+				<?php endif; // header image was removed (again) ?>
+
+		        </div>
+					<?php sparkling_header_menu(); ?>
+					</div>
+		    </div>
+		  </div>
+		</nav><!-- .site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
+
+			<div class="top-section">
+				<?php sparkling_featured_slider(); ?>
+				<?php sparkling_call_for_action(); ?>
+			</div>
+
+		<div class="container main-content-area">
+			<div class="row">
+				<div id="content" class="main-content-inner col-sm-12 col-md-8 <?php echo of_get_option( 'site_layout' ); ?>">
